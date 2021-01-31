1b1c3ba272fbefabbce5128d154b4b43afdb7ae6
James Hrivnak <jameshrivnak4@gmail.com>
2021-01-31 01:02:10 -0700

    tic-tac-toe complete

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,12 +2,24 @@ const gamePlayState = {
   hasAlreadyWon: false,
   xPlayerWinCount: 0,
   oPlayerWinCount: 0,
-  xHasFirstMove: true
+  currentMove: 'X',
+  xHasFirstMove: true,
+  confirmed: {
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    8: false,
+    9: false
+  }
 }
 const gamePlayMethods = {
   handleUserEntry: (e) => {
     e.preventDefault();
-    if (gamePlayState.hasAlreadyWon) {
+    if (gamePlayState.hasAlreadyWon || gamePlayState.confirmed[event.target.id]) {
       return;
     }
     var player = gamePlayMethods.countMoves(e);
@@ -16,27 +28,36 @@ const gamePlayMethods = {
   },
 
   countMoves: (e) => {
-    //Opponents moves cannot be overwritten...
-    if (document.getElementById(e.target.id).innerHTML !== '') {
-      return;
-    }
+    //Opponents moves cannot be overwritten.
+    gamePlayState.confirmed[e.target.id] = true;
+    // if (gamePlayState.confirmed[e.target.id]) {
+    //   return;
+    // }
+    // console.log('hello');
     var boxes = document.getElementsByClassName('boxes');
-    let xLength = Array.from(boxes).filter(item => item.innerHTML.includes('X') || item.innerHTML.includes('O')).length;
+    let xLength = Array.from(boxes).filter(item => (item.innerHTML.includes('X') || item.innerHTML.includes('O')) && gamePlayState.confirmed[item.id] === true).length
 
+    console.log(gamePlayState.xHasFirstMove);
     if (gamePlayState.xHasFirstMove === false) {
-      if (xLength % 2 === 0) {
+      console.log('test');
+      if (xLength % 2 !== 0) {
         document.getElementById(e.target.id).innerHTML = 'O';
+        gamePlayState.currentMove = 'X';
         return 'O';
       } else {
+        console.log('here')
         document.getElementById(e.target.id).innerHTML = 'X';
+        gamePlayState.currentMove = 'O';
         return 'X';
       }
     } else {
-      if (xLength % 2 === 0) {
+      if (xLength % 2 !== 0) {
         document.getElementById(e.target.id).innerHTML = 'X';
+        gamePlayState.currentMove = 'O';
         return 'X';
       } else {
         document.getElementById(e.target.id).innerHTML = 'O';
+        gamePlayState.currentMove = 'X';
         return 'O';
       }
     }
@@ -47,6 +68,8 @@ const gamePlayMethods = {
     var boxes = document.getElementsByClassName('boxes');
     Array.from(boxes).forEach(box => {
       if (box.innerHTML.includes(player)) {
+        box.style.backgroundColor = 'seagreen';
+        box.style.borderColor = 'yellowgreen'
         var horizontalWin, verticalWin, rightDiagonalWin, leftDiagonalWin = false;
 
         if (box.id === '1' || box.id === '4' || box.id === '7') {
@@ -80,6 +103,7 @@ const gamePlayMethods = {
     p === 'X' ? gamePlayState.xPlayerWinCount += 1 : gamePlayState.oPlayerWinCount += 1;
     p === 'X' ? winner.innerHTML = gamePlayState.xPlayerWinCount : winner.innerHTML = gamePlayState.oPlayerWinCount;
     p === 'X' ? gamePlayState.xHasFirstMove = true : gamePlayState.xHasFirstMove = false;
+    p === 'X' ? gamePlayState.currentMove = 'X' : gamePlayState.currentMove = 'O';
     gamePlayMethods.refreshBoard();
   },
 
@@ -87,9 +111,11 @@ const gamePlayMethods = {
     gamePlayState.hasAlreadyWon = false;
     document.querySelectorAll('.boxes').forEach(box => {
       box.innerHTML = '';
+      box.style = 'intial';
+      gamePlayState.confirmed[box.id] = false;
     })
+    adjustBoardWidth();
   }
-
 }
 
 const detectWinners = {
@@ -133,23 +159,34 @@ const detectWinners = {
 const nameEntry = {
   xPlayer: undefined,
   oPlayer: undefined,
+  boardSize: undefined,
 
   handlePlayerNameEntry: (e) => {
     e.preventDefault();
+    if (isNaN(event.target['2'].value)) {
+      alert('Please enter a number');
+      return;
+    }
     nameEntry.xPlayer = event.target['0'].value || 'X Player';
     nameEntry.oPlayer = event.target['1'].value || 'O Player';
+    nameEntry.boardSize = event.target['2'].value || 510;
+
+    adjustBoardWidth(nameEntry.boardSize);
 
     document.getElementById('p1-name-header').childNodes[0].nodeValue = nameEntry.xPlayer + ' ';
 
     document.getElementById('p2-name-header').childNodes[0].nodeValue = nameEntry.oPlayer + ' ';
-
 
     nameEntry.disableInputs();
   },
 
   disableInputs: () => {
     document.querySelectorAll('.player-name-input').forEach(input => {
-      input.disabled = true;
+    if (input.id !== 'board-size') {
+        input.disabled = true;
+        input.hidden = true;
+      }
+  document.querySelector('.enter-players').classList.add('input-width-adjusted');
     })
   }
 }
@@ -173,32 +210,64 @@ const gameListeners = {
     });
   },
   enterPlayers: () => {
-    const players = document.getElementById('enter-players');
+    const players = document.querySelector('.enter-players');
     players.addEventListener('submit', event => {
       nameEntry.handlePlayerNameEntry(event);
     });
+  },
+  detectPlayerMove: () => {
+    const boxes = document.querySelectorAll('.boxes');
+    boxes.forEach(box => {
+      box.addEventListener('mouseover', event => {
+        potentialMoveReveal(box);
+      }),
+      box.addEventListener('mouseout', event => {
+        potentialMoveRemoval(box);
+      })
+    })
+  },
+
+
+}
+
+var potentialMoveReveal = (box) => {
+  if (gamePlayState.confirmed[box.id] !== true) {
+    box.innerHTML = gamePlayState.currentMove;
   }
 }
 
-var updateBoardWidth = (event) => {
-
+var potentialMoveRemoval = (box) => {
+  if (gamePlayState.confirmed[box.id] !== true) {
+    box.innerHTML = '';
+  }
 }
 
-var adjustBoardWidth = () => {
-  var boardWidth = document.getElementById('1').clientWidth;
-  document.getElementById('board').style.width = ((boardWidth * 3 + 6) + 'px');
+var adjustBoardWidth = (inputSize) => {
+  var boardWidth =  (inputSize / 3) || document.getElementById('1').clientWidth;
+
+  document.getElementById('board').style.height = inputSize + 'px';
+
+  document.getElementById('board').style.width = (boardWidth * 3) + 6 + 'px';
 
   document.querySelectorAll('.boxes').forEach((box) => {
     box.style.height = boardWidth + 'px';
     box.style.width = boardWidth + 'px';
     box.style.lineHeight = boardWidth + 'px';
-  })
-
-  document.getElementById('new-game-button').style.width = ((boardWidth * 3 + 8) + 'px');
+    box.style.fontSize = (boardWidth / 1.5) + 'px';
+  });
 
   document.getElementById('player-headers').style.width = ((boardWidth * 3 + 8) + 'px');
 
-  document.getElementById('enter-players').style.width = ((boardWidth * 3 + 8) + 'px');
+
+  document.getElementById('new-game-button').style.width = ((boardWidth * 3 + 8) + 'px');
+
+  document.querySelector('.enter-players').style.width = ((boardWidth * 3 + 8) + 'px');
+
+  var test = document.getElementsByTagName('p').length;
+
+
+
+  // document.getElementById('body').style.height = ((boardWidth * 3) + 6  + newGameButtonHeight) + 'px'
 }
 
 const addEventListeners = () => {

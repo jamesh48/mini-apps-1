ccce1cc251a985461fcc3e974c8af28847361f90
James Hrivnak <jameshrivnak4@gmail.com>
2021-02-19 21:24:42 -0700

    horizontal and vertical functions now work for 6x7 board

--- challenge_4/client/src/components/square.jsx
+++ challenge_4/client/src/components/square.jsx
@@ -6,8 +6,7 @@ const Square = (props) => {
 
   return (
     <div className= {'square ' + test}
-
-    id={index + 1} data-tile= {index + 1} data-y={(index % 4) + 1} data-x={Math.ceil((index + 1) / 4)} onClick=
+    id={index + 1} data-tile= {index + 1} data-y={(index % 7) + 1} data-x={Math.ceil((index + 1) / 7)} onClick=
       {handleClick}>
     </div>
   )
--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -12,7 +12,7 @@ class App extends React.Component {
     this.detectHorizontalWin = this.detectHorizontalWin.bind(this);
     this.state = {
       turn: 'red',
-      board: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+      board: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42],
       t1: '',
       t2: '',
       t3: '',
@@ -28,7 +28,33 @@ class App extends React.Component {
       t13: '',
       t14: '',
       t15: '',
-      t16: ''
+      t16: '',
+      t17: '',
+      t18: '',
+      t19: '',
+      t20: '',
+      t21: '',
+      t22: '',
+      t23: '',
+      t24: '',
+      t25: '',
+      t26: '',
+      t27: '',
+      t28: '',
+      t29: '',
+      t30: '',
+      t31: '',
+      t32: '',
+      t33: '',
+      t34: '',
+      t35: '',
+      t36: '',
+      t37: '',
+      t38: '',
+      t39: '',
+      t40: '',
+      t41: '',
+      t42: ''
     }
   }
 
@@ -43,9 +69,11 @@ class App extends React.Component {
   handleClick(event) {
     const currentTile = event.target.dataset;
 
-    let i = 16 - (4 - currentTile.y)
-    while (this.state['t' + i.toString()] !== '' && (i - 4) > 0) {
-      i -= 4;
+    let i = 42 - (7 - currentTile.y)
+    console.log(i);
+
+    while (this.state['t' + i.toString()] !== '' && (i - 7) > 0) {
+      i -= 7;
     }
 
     this.setState(prevState => {
@@ -60,17 +88,17 @@ class App extends React.Component {
         }
       }
     }, () => {
-      let dVW = this.detectVerticalWin()
+      let dVW = this.detectVerticalWin();
       let dHW = this.detectHorizontalWin(i);
     });
   }
 
   detectVerticalWin() {
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= 42; i++) {
       const token = this.state['t' + i];
-      const t2v = (this.state['t' + (i + 4)] === token);
-      const t3v = (this.state['t' + (i + 8)] === token);
-      const t4v = (this.state['t' + (i + 12)] === token);
+      const t2v = (this.state['t' + (i + 7)] === token);
+      const t3v = (this.state['t' + (i + 14)] === token);
+      const t4v = (this.state['t' + (i + 21)] === token);
 
       if (token && t2v && t3v && t4v) {
         console.log('vertical victory')
@@ -79,27 +107,22 @@ class App extends React.Component {
     }
   }
 
-detectHorizontalWin(i) {
-  let column = (i % 4) || 4;
-  let rowStart = (i - column + 1)
-  const rowEnd = rowStart + 3;
-  console.log(rowEnd, rowStart)
-  for (let i = rowStart; i <= rowEnd; i++) {
-    const token = this.state['t' + i];
-    const t2h = (this.state['t' + (i + 1)] === token && (i + 1) <= rowEnd);
-    const t3h = (this.state['t' + (i + 2)] === token && (i + 2) <= rowEnd);
-    const t4h = (this.state['t' + (i + 3)] === token && (i + 3) <= rowEnd);
-    if (token && t2h && t3h && t4h) {
-      console.log(`horizontal victory!`)
-      break;
+  detectHorizontalWin(i) {
+    let column = (i % 7) || 7;
+    let rowStart = (i - column + 1)
+    const rowEnd = rowStart + 6;
+    console.log(rowEnd, rowStart)
+    for (let i = rowStart; i <= rowEnd; i++) {
+      const token = this.state['t' + i];
+      const t2h = (this.state['t' + (i + 1)] === token && (i + 1) <= rowEnd);
+      const t3h = (this.state['t' + (i + 2)] === token && (i + 2) <= rowEnd);
+      const t4h = (this.state['t' + (i + 3)] === token && (i + 3) <= rowEnd);
+      if (token && t2h && t3h && t4h) {
+        console.log(`horizontal victory!`)
+        break;
+      }
     }
   }
-}
-
-// 1,2,3,4 = 10
-// 2,3,4,5 = 14
-// 5,6,7,8 = 26
-
 
   render() {
     const { renderSquares, handleClick } = this;
